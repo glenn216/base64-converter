080fc43a7df5a2e2c69b60e9cebacd7e996be044
Glenn Alon <glennalon216@hotmail.com>
2019-02-27 11:48:10 +0800

    first commit

---
+++ Base64 Converter/MainForm.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: glenn
+ * Date: 24/02/2019
+ * Time: 11:07 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Base64_Converter
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+            var TextToBase64 = System.Text.Encoding.UTF8.GetBytes(textBox1.Text);
+            textBox2.Text = System.Convert.ToBase64String(TextToBase64);
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+	        var Base64ToText = System.Convert.FromBase64String(textBox2.Text);
+            textBox1.Text =  System.Text.Encoding.UTF8.GetString(Base64ToText);
+		}		
+	}
+}
---
+++ Base64 Converter/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: glenn
+ * Date: 24/02/2019
+ * Time: 11:07 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace Base64_Converter
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
